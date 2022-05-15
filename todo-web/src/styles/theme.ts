11387b8eae1a46fb405cba823f731fe49b8fc2f4
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2022-05-15 18:01:21 -0300

    fix: fixed global stuffs

--- todo-web/src/styles/theme.ts
+++ todo-web/src/styles/theme.ts
@@ -2,7 +2,9 @@ export default {
   colors: {
     white: '#ffffff',
     black: '#000000',
-    grey: '#f0f0f0',
-    pink: '#EC91D8'
+    grey: '#edf2f4',
+    pink: '#EC91D8',
+    darkBlue: '#2b2d42',
+    lightBlue: '#8d99ae'
   }
 }
