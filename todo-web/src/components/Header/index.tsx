cd043ecff29e26c9932d836e3a239069b397ce4a
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2022-05-15 18:01:45 -0300

    feat: created header component

---
+++ todo-web/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './styles'
+
+const Header = () => {
+  return (
+    <S.Wrapper>
+      <S.Title>
+        Tasks <p>Lists</p>
+      </S.Title>
+    </S.Wrapper>
+  )
+}
+
+export default Header
---
+++ todo-web/src/components/Header/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+import theme from '../../styles/theme'
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 8rem 0;
+
+  &::after {
+    content: '';
+    width: 100%;
+    height: 2px;
+    background-color: ${theme.colors.lightBlue};
+  }
+
+  &::before {
+    content: '';
+    width: 10rem;
+    height: 2px;
+    background-color: ${theme.colors.lightBlue};
+  }
+`
+
+export const Title = styled.span`
+  display: flex;
+  align-items: center;
+  font-size: 2.8rem;
+  color: ${theme.colors.darkBlue};
+  font-weight: 700;
+  padding: 0 2.2rem;
+
+  p {
+    font-weight: 300;
+    color: ${theme.colors.lightBlue};
+    padding-left: 1rem;
+  }
+`
