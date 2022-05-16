866c594b866d85fb412abfbae7ce43d6af65b09b
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2022-05-16 08:18:59 -0300

    feat: created dashboard component

---
+++ todo-web/src/components/Dashboard/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  max-width: 122rem;
+  margin: 0 auto;
+`
+
+export const TasksWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  div:first-child {
+    margin-right: 2rem;
+  }
+`
