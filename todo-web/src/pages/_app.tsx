31a01d98c3dc11f72c53353a6e4a73540cde5215
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2022-05-20 23:11:00 -0300

    feat: apollo client setup and created task query

--- todo-web/src/pages/_app.tsx
+++ todo-web/src/pages/_app.tsx
@@ -1,12 +1,14 @@
+import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 import GlobalStyle from '../styles/global'
+import { client } from '../utils/apollo'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ApolloProvider client={client}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ApolloProvider>
   )
 }
 
