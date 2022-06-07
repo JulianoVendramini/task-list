dfdb6b7a05690051b691882119062ddd4f7bf909
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2022-06-07 20:02:58 -0300

    feat: added handle check and update task

--- todo-web/src/components/Task/index.tsx
+++ todo-web/src/components/Task/index.tsx
@@ -6,21 +6,25 @@ import * as S from './styles'
 
 type Props = {
   task: TaskProps
-  handleClick: (id: number) => void
+  handleCheck: (id: number, isDone: boolean) => void
   handleRemove: (id: number) => void
+  handleUpdate: (id: number) => void
 }
 
-const Task = ({ task, handleClick, handleRemove }: Props) => {
+const Task = ({ task, handleCheck, handleRemove, handleUpdate }: Props) => {
   return (
     <S.Wrapper>
-      <S.Title isDone={task.isDone} onClick={() => handleClick(task.id)}>
+      <S.Title
+        isDone={task.isDone}
+        onClick={() => handleCheck(task.id, !task.isDone)}
+      >
         {task.title}
       </S.Title>
       <S.ActionButtonWrapper>
         <S.RemoveButton onClick={() => handleRemove(task.id)}>
           <FiTrash />
         </S.RemoveButton>
-        <S.UpdateButton>
+        <S.UpdateButton onClick={() => handleUpdate(task.id)}>
           <BsFillPencilFill />
         </S.UpdateButton>
       </S.ActionButtonWrapper>
