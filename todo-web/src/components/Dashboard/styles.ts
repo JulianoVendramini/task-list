dacbd5c2a5f1f9428cf38c8c46a84e52d8c6ba90
Juliano Vendramini Codognos <66084938+JulianoVendramini@users.noreply.github.com>
2022-06-07 20:02:17 -0300

    feat: styled dashboard

--- todo-web/src/components/Dashboard/styles.ts
+++ todo-web/src/components/Dashboard/styles.ts
@@ -3,10 +3,12 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
   max-width: 122rem;
   margin: 0 auto;
+  padding-bottom: 10rem;
 `
 
 export const TasksWrapper = styled.div`
   width: 100%;
-  display: flex;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 5rem;
 `
